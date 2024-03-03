450600d535cb2bc3b9304c5726e6753a1790c137
khokh <khoiskyi.oleksandr@chnu.edu.ua>
2024-03-03 12:40:10 +0200

    1-2 done

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿namespace Lab9_10CharpT
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Task1");
+            ReverseCheck reverseCheck = new ReverseCheck();
+            reverseCheck.Run();
 
+            Console.WriteLine("\nTask2\n");
+            PositiveNegativeOrder pnOrder = new PositiveNegativeOrder();
+            pnOrder.Run();
+        }
+    }
+}
